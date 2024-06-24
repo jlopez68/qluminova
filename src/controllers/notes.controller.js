f1b64df8cd6bf32f14177b2a12eb7fbdc2cd185f
jlopez68 <jllp68@gmail.com>
2024-06-24 10:07:53 -0500

    pais2

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -439,11 +439,12 @@ const notes100 = await User.findOne( {email:ver1}).lean();
 export const calcularposicion = async (req, res) => {
 
   Usuario.find({tipo_usuario:"Jugador", pais:"Ecuador"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion1);
-  Usuario.find({tipo_usuario:"Jugador", pais:"Bolivia"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion1);
-  Usuario.find({tipo_usuario:"Jugador", pais:"Colombia"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion1);
-  Usuario.find({tipo_usuario:"Jugador", pais:"Peru"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion1);
+  Usuario.find({tipo_usuario:"Jugador", pais:"Bolivia"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion2);
+  Usuario.find({tipo_usuario:"Jugador", pais:"Colombia"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion3);
+  Usuario.find({tipo_usuario:"Jugador", pais:"Peru"},{_id:0,email:1}).sort({ puntos: "desc" }).lean().exec(posicion4);
   req.flash("success_msg", "Posicion Actualizada");
   res.redirect("/notes");     
+
   async function  posicion1 (err, _id) {
   
    if(err) {
@@ -475,7 +476,101 @@ export const calcularposicion = async (req, res) => {
     
    }
   }
+  async function  posicion2 (err, _id) {
   
+    if(err) {
+      console.log("error");
+      console.log(err);
+    }
+    const ciclo = _id.length;
+    console.log("ciclo2",ciclo);
+     console.log(_id[0]);
+    for (let step = 0; step < ciclo; step++) {
+      // Runs 5 times, with values of step 0 through 4.
+   //       console.log(_id.usuario[0])  
+      let ver = JSON.stringify(_id[step]); 
+      var ver1 = ver.slice(10,-2);
+      
+   console.log(ver);
+   console.log(ver1);
+   const notes100 = await User.findOne( {email:ver1}).lean();
+     const pos = step+1;   
+     console.log("pos1",pos);
+     console.log("email",notes100.email);
+        
+     const doc2 = await Usuario.updateOne({email: notes100.email},  {posicion:pos});
+     if(err) {
+       console.log("error");
+       console.log(err);
+     }
+     console.log("aca entre");
+     
+    }
+   }
+   async function  posicion3 (err, _id) {
+  
+    if(err) {
+      console.log("error");
+      console.log(err);
+    }
+    const ciclo = _id.length;
+    console.log("ciclo2",ciclo);
+     console.log(_id[0]);
+    for (let step = 0; step < ciclo; step++) {
+      // Runs 5 times, with values of step 0 through 4.
+   //       console.log(_id.usuario[0])  
+      let ver = JSON.stringify(_id[step]); 
+      var ver1 = ver.slice(10,-2);
+      
+   console.log(ver);
+   console.log(ver1);
+   const notes100 = await User.findOne( {email:ver1}).lean();
+     const pos = step+1;   
+     console.log("pos1",pos);
+     console.log("email",notes100.email);
+        
+     const doc2 = await Usuario.updateOne({email: notes100.email},  {posicion:pos});
+     if(err) {
+       console.log("error");
+       console.log(err);
+     }
+     console.log("aca entre");
+     
+    }
+   }
+ 
+   async function  posicion4 (err, _id) {
+  
+    if(err) {
+      console.log("error");
+      console.log(err);
+    }
+    const ciclo = _id.length;
+    console.log("ciclo2",ciclo);
+     console.log(_id[0]);
+    for (let step = 0; step < ciclo; step++) {
+      // Runs 5 times, with values of step 0 through 4.
+   //       console.log(_id.usuario[0])  
+      let ver = JSON.stringify(_id[step]); 
+      var ver1 = ver.slice(10,-2);
+      
+   console.log(ver);
+   console.log(ver1);
+   const notes100 = await User.findOne( {email:ver1}).lean();
+     const pos = step+1;   
+     console.log("pos1",pos);
+     console.log("email",notes100.email);
+        
+     const doc2 = await Usuario.updateOne({email: notes100.email},  {posicion:pos});
+     if(err) {
+       console.log("error");
+       console.log(err);
+     }
+     console.log("aca entre");
+     
+    }
+   }
+ 
   };
 
 
